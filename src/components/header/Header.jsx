22de1b46fc5a62366a1534334dae9ee8de6a7e68
Mscalzi <104732019+Mscalzi@users.noreply.github.com>
2022-05-16 11:01:57 -0700

    updated procfile

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,22 +1,22 @@
 import React from 'react'
 import './header.css'
 import Cta from './Cta'
-import me from '../../assets/me4.png'
+// import me from '../../assets/me4.png'
 import HeaderSocials from './HeaderSocials'
-import {HiChevronDoubleRight} from 'react-icons/hi'
+// import {HiChevronDoubleRight} from 'react-icons/hi'
 
 const Header = () => {
   return (
-    <header>
+    <header id='header'>
       <div className="container header__container">
-        <h5>Hello I'm</h5>
+        <h2>Hello I'm</h2>
         <h1>Matthew L. Scalzi</h1>
-        <h5 className="text-light">Full Stack Developer</h5>
+        <h2 className="text-light">Full Stack Developer</h2>
         <Cta />
         <HeaderSocials />
-        <div className='me'>
+        {/* <div className='me'>
           <img className='me' src={me} alt='me'></img>
-        </div>
+        </div> */}
         {/* <a href="#contact" className='scroll__down'>Scroll Down<HiChevronDoubleRight className='arrow' /></a> */}
       </div>
     </header>
