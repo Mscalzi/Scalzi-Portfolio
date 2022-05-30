95c57cf7891de0aa0269a9d142472bf68a3b5fc3
Mscalzi <104732019+Mscalzi@users.noreply.github.com>
2022-05-30 15:47:00 -0700

    updated

--- src/components/header/Cta.jsx
+++ src/components/header/Cta.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import resume from '../../assets/Resume.pdf'
+import resume from '../../assets/Resume3final.pdf'
 const Cta = () => {
   return (
     <div className='cta'>
